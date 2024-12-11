7cb26a087fe7865632eb87d5a3ec20debe8a11ee
ZenXaris03 <it2021101@hua.gr>
2024-12-11 17:16:13 +0200

    Admin demo part 1. To write templates.

--- code/myfaculty/phds/urls.py
+++ code/myfaculty/phds/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 from . import views
 
 app_name = 'phds'
+# urlpatterns = [
+#     path('sec/phd/new', views.sec_create_phd_student.as_view(), name='sec_create_phd_student'),
+#     path('sec/phds', views.sec_list_phd_students.as_view(), name='sec_list_phd_students'),
+#     path('sec/phd/<int:pk>', views.sec_edit_phd_student.as_view(), name='sec_edit_phd_student'),
+#     path('sec/phd/<int:pk>/delete', views.sec_delete_phd_student.as_view(), name='sec_delete_phd_student'),
+#     path('staff/phds', views.staff_list_phd_students.as_view(), name='staff_list_phd_students'),
+#     path('phd/<int:pk>/profile', views.phd_student_profile.as_view(), name='phd_student_profile'),
+#     path('phd/<int:pk>', views.phd_student_edit.as_view(), name='phd_student_edit'),
+# ]
+
+
+# Demo urls for Admin ... To be deleted
 urlpatterns = [
-    path('sec/phd/new', views.sec_create_phd_student.as_view(), name='sec_create_phd_student'),
-    path('sec/phds', views.sec_list_phd_students.as_view(), name='sec_list_phd_students'),
-    path('sec/phd/<int:pk>', views.sec_edit_phd_student.as_view(), name='sec_edit_phd_student'),
-    path('sec/phd/<int:pk>/delete', views.sec_delete_phd_student.as_view(), name='sec_delete_phd_student'),
-    path('staff/phds', views.staff_list_phd_students.as_view(), name='staff_list_phd_students'),
-    path('phd/<int:pk>/profile', views.phd_student_profile.as_view(), name='phd_student_profile'),
-    path('phd/<int:pk>', views.phd_student_edit.as_view(), name='phd_student_edit'),
+    path('create/', views.phd_create, name='phd_create'),
+    path('list/', views.phd_list, name='phd_list'),
+    path('update/<int:id_number>/', views.phd_update, name='phd_update'),
+    path('delete/<int:id_number>/', views.phd_delete, name='phd_delete'),
 ]
--- code/myfaculty/phds/views.py
+++ code/myfaculty/phds/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import PhDStudent
+from .forms import PhDStudentForm
 from django.views import generic
 from myprofile.checks import is_staff_member, is_secreteriat
 
@@ -11,43 +12,85 @@ from myprofile.checks import is_staff_member, is_secreteriat
 
 # Secreatary can CRUD everything
 
-# Create View - Secretary
-class sec_create_phd_student():
-    def dumb_return(self):
-        print("sec_create_phd_student")
+# # Create View - Secretary
+# class sec_create_phd_student():
+#     def dumb_return(self):
+#         print("sec_create_phd_student")
     
-# Read View - Secretary
-class sec_list_phd_students():
-    def dumb_return(self):
-        print("sec_list_phd_students")
+# # Read View - Secretary
+# class sec_list_phd_students():
+#     def dumb_return(self):
+#         print("sec_list_phd_students")
     
-# Upadate View - Secretary
-class sec_edit_phd_student():
-    def dumb_return(self):
-        print("sec_edit_phd_student")
+# # Upadate View - Secretary
+# class sec_edit_phd_student():
+#     def dumb_return(self):
+#         print("sec_edit_phd_student")
     
-# Delete View - Secretary
-class sec_delete_phd_student():
-    def dumb_return(self):
-        print("sec_delete_phd_student")
+# # Delete View - Secretary
+# class sec_delete_phd_student():
+#     def dumb_return(self):
+#         print("sec_delete_phd_student")
 
 
-# Staff Members of a PhD student can only see (Read) the PhD's student data
+# # Staff Members of a PhD student can only see (Read) the PhD's student data
 
-# Read View - Staff Member
-class staff_list_phd_students():
-    def dumb_return(self):
-        print("staff_list_phd_students")
+# # Read View - Staff Member
+# class staff_list_phd_students():
+#     def dumb_return(self):
+#         print("staff_list_phd_students")
 
 
-# PhD student can only see (Read) their profile and add a picture (Update)
+# # PhD student can only see (Read) their profile and add a picture (Update)
 
-# Read View - PhD Student
-class phd_student_profile():
-    def dumb_return(self):
-        print("phd_student_profile")
+# # Read View - PhD Student
+# class phd_student_profile():
+#     def dumb_return(self):
+#         print("phd_student_profile")
 
-# Update View - PhD Student
-class phd_student_edit():
-    def dumb_return(self):
-        print("phd_student_edit")
+# # Update View - PhD Student
+# class phd_student_edit():
+#     def dumb_return(self):
+#         print("phd_student_edit")
+
+
+
+
+
+
+
+# Admin CRUD Demo ... To be deleted
+
+# Create View
+def phd_create(request):
+    form = PhDStudentForm()
+    if request.method == 'POST':
+        form = PhDStudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_phd_list')
+    return render(request, 'phds/admin_phd_create.html', {'form':form})
+
+# Read View
+def phd_list(request):
+    phds = PhDStudent.objects.all()
+    return render(request, 'phds/admin_phd_list.html', {'phds':phds})
+
+# Update View
+def phd_update(request, id_number):
+    phd = PhDStudent.objects.get(id_number=id_number)
+    form = PhDStudentForm()
+    if request.method == 'POST':
+        form = PhDStudentForm(request.POST, instance=phd)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_phd_list')
+    return render(request, 'phds/admin_phd_create.html', {'form':form})
+
+# Delete View
+def phd_delete(request, id_number):
+    phd = PhDStudent.objects.get(id_number=id_number)
+    if request.method == 'POST':
+        phd.delete()
+        return redirect('admin_phd_list')
+    return render(request, 'phds/phd_confirm_delete.html', {'phd':phd})
