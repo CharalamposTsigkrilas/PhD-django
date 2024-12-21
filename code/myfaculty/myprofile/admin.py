55fe721b14caa724668740810316a20844edce9c
ZenXaris03 <it2021101@hua.gr>
2024-12-21 20:10:21 +0200

    phdstudent for admin page moved from phds to myprofile app

--- code/myfaculty/myprofile/admin.py
+++ code/myfaculty/myprofile/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import StaffMember, Associate, CustomUserPermissions, Student
+from .models import StaffMember, Associate, CustomUserPermissions, Student, PhDStudent
 # Register your models here.
 admin.site.register(StaffMember)
 admin.site.register(Associate)
 admin.site.register(CustomUserPermissions)
 admin.site.register(Student)
+admin.site.register(PhDStudent)
