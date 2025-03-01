b6203a61b31fae67470b67c9dd1efe32669b217c
ZenXaris03 <it2021101@hua.gr>
2025-03-01 18:11:22 +0200

    Models for Journals, Conferenses and Teaching created in phds App

--- code/myfaculty/phds/models.py
+++ code/myfaculty/phds/models.py
@@ -1,3 +1,47 @@
 from django.db import models
-
+from myprofile.models import StaffMember, PhdStudent
+from curricula.models import Course
 # Create your models here.
+
+class Journal(models.Model):
+    candidate = models.ForeignKey(PhdStudent, null=True, on_delete=models.SET_NULL)
+
+    title = models.TextField()
+    authorsList = models.TextField()
+    hasSupervisor = models.BooleanField()
+    journal = models.CharField()
+    publisher = models.CharField()
+    volume = models.CharField()
+    issue = models.CharField()
+    year = models.IntegerField()
+    doi = models.CharField()
+
+class Conferense(models.Model):
+    candidate = models.ForeignKey(PhdStudent, null=True, on_delete=models.SET_NULL)
+
+    ttitle = models.TextField()
+    authorsList = models.TextField()	
+    conferenceName = models.CharField()	
+    venue = models.CharField()
+    year = models.IntegerField()
+    hasSupervisor = models.BooleanField()
+
+class Teaching(models.Model):
+    candidate = models.ForeignKey(PhdStudent, null=True, on_delete=models.SET_NULL)
+    faculty = models.ForeignKey(StaffMember, null=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
+
+    year = models.IntegerField()
+    teachingType = {
+        THEORY_LECTURE : "Διάλεξη Θεωρίας",
+        LABORATORY_COURSE : "Εργαστηριακό Μάθημα",
+        ASSIGNMENT_CORRECTION : "Διόρθωση Εργασίας",
+        TUTORIAL_COURSE : "Φροντιστηριακό Μάθημα"
+    } 
+    hoursPerWeek = models.IntegerField()	
+    noWeeks = models.IntegerField()	
+    haveContract = models.BooleanField()
+    comments = models.TextField()
+
+    approvedByFaculty = models.BooleanField()
+    approvedDate = models.DateField()
