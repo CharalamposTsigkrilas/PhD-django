dadd449e063ca5472925f5a1a8c86443ee7b4101
ZenXaris03 <it2021101@hua.gr>
2025-03-08 19:36:54 +0200

    List Views for phds updated.

--- code/myfaculty/phds/urls.py
+++ code/myfaculty/phds/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'phds'
 urlpatterns = [
-    path('sec/phd/journals', views.sec_list_journals.as_view(), name='sec_list_journals'),
-    path('sec/phd/conferences', views.sec_list_conferences.as_view(), name='sec_list_conferences'),
-    path('sec/phd/teachings', views.sec_list_teachings.as_view(), name='sec_list_teachings'),
+    path('sec/journals', views.sec_list_journals.as_view(), name='sec_list_journals'),
+    path('sec/conferences', views.sec_list_conferences.as_view(), name='sec_list_conferences'),
+    path('sec/teachings', views.sec_list_teachings.as_view(), name='sec_list_teachings'),
 
-    path('staff/phd/journals', views.staff_list_journals.as_view(), name='staff_list_journals'),
-    path('staff/phd/conferences', views.staff_list_conferences.as_view(), name='staff_list_conferences'),
-    path('staff/phd/teachings', views.staff_list_teachings.as_view(), name='staff_list_teachings'),
+    path('staff/journals', views.staff_list_journals.as_view(), name='staff_list_journals'),
+    path('staff/conferences', views.staff_list_conferences.as_view(), name='staff_list_conferences'),
+    path('staff/teachings', views.staff_list_teachings.as_view(), name='staff_list_teachings'),
 
     path('phd/journals', views.phd_list_journals.as_view(), name='phd_list_journals'),
     path('phd/conferences', views.phd_list_conferences.as_view(), name='phd_list_conferences'),
-    path('phd/teachings', views.phd_list_teachings.as_view(), name='phd_list_teachings')
+    path('phd/teachings', views.phd_list_teachings.as_view(), name='phd_list_teachings'),
 ]
